20bf0b74d8635b04209cf2c125144e43e62069ea
oppen123 <161986254+oppen123@users.noreply.github.com>
2024-07-06 02:19:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,25 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf luci-app-argon-config
+rm -rf feeds/packages/lang/golang
+
+# golong 
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
+# argon主题
+git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/downloads/luci-theme-argon
+
+# Mosdns
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+# kenzo源
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
