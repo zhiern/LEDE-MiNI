2ae082556f08bed366ebcaa2e8f0133242aed18c
oppen321 <zj18139624826@gmail.com>
2024-07-06 23:01:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -47,6 +47,7 @@ git clone -b main https://github.com/linkease/istore package/istoreos
 # Adguardhome
 git clone -b main https://github.com/oppen321/luci-app-adguardhome package/luci-app-adguardhome
 
+# OpenAppFilter应用过滤
 git clone -b main https://github.com/oppen321/OpenAppFilter package/OpenAppFilter
 ./scripts/feeds update -a
 ./scripts/feeds install -a
